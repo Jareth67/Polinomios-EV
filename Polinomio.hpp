7700fd8307d90fbbb62f10d85abcc06048b67bfc
Jareth67 <a224214671@unison.mx>
2025-04-12 21:16:03 -0700

    Create Polinomio.hpp

---
+++ Polinomio.hpp
@@ -0,0 +1,9 @@
+#ifndef POLINOMIO_HPP
+#define POLINOMIO_HPP
+
+#include <vector>
+
+double eval_standard(const std::vector<double>& coef, double x);
+double eval_horner(const std::vector<double>& coef, double x);
+
+#endif // POLINOMIO_HPP
